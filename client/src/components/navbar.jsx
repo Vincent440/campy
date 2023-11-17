5753c9093ad651dcc4ccd86100d3aa376e2e8f52
Vincent440 <vinceshury@gmail.com>
2023-11-16 21:32:22 -0500

    Updated navbar links to conditionally render

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,7 +1,32 @@
 import { NavLink } from 'react-router-dom'
 // NavLink applies the 'active' class styling to the currently visited route.
-
+import Auth from '../utils/auth'
 const Navbar = () => {
+  const renderLoginLinks = () => {
+    // If users logged in display a logout link that refreshes the page by not using a react-router-dom link after calling logout,
+    if (Auth.loggedIn()) {
+      return (
+        <li>
+          <a href='/login' onClick={() => Auth.logout()}>
+            Logout
+          </a>
+        </li>
+      )
+    } else {
+      // If the user is not logged in show a Login link or a Sign Up link
+      return (
+        <>
+          <li>
+            <NavLink to='/login'>Login</NavLink>
+          </li>
+          <li>
+            <NavLink to='/sign-up'>Sign Up</NavLink>
+          </li>
+        </>
+      )
+    }
+  }
+
   return (
     <nav>
       <span>Campy &copy; </span>
@@ -11,12 +36,7 @@ const Navbar = () => {
             Dashboard
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/login'>Login</NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up'>Sign Up</NavLink>
-        </li>
+        {renderLoginLinks()}
       </ul>
     </nav>
   )
