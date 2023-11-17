b08de0373b6e669821552d9d6e870917ee10059f
Vincent440 <vinceshury@gmail.com>
2023-11-16 21:32:43 -0500

    Basic login page working with email and password

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,13 +1,55 @@
-// import { useMutation } from '@apollo/client'
-// import { LOGIN_USER } from '../utils/mutations'
-// import Auth from '../utils/auth';
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client'
+
+import { LOGIN_USER } from '../utils/mutations'
+import Auth from '../utils/auth'
 
 const Login = () => {
-  // const [loginUser, { error }] = useMutation(LOGIN_USER)
+  const [loginFormState, setLoginFormState] = useState({ email: '', password: '' })
 
+  const [loginUser, { error }] = useMutation(LOGIN_USER)
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const mutationResponse = await loginUser({
+        variables: { email: loginFormState.email, password: loginFormState.password },
+      })
+      const token = mutationResponse.data.login.token
+      Auth.login(token)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setLoginFormState({
+      ...loginFormState,
+      [name]: value,
+    })
+  }
+  /* 
+ TODO:  Remove <br /> elements when adding styling 
+*/
   return (
     <div>
-      <h1>Login!</h1>
+      <h1>Login</h1>
+      <p>Don&apos;t have an account? <Link to='/sign-up'>Sign Up</Link></p>
+
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor='email-login'>Email: </label>
+        <input required onChange={handleChange} type='email' name='email' id='email-login' />
+        <br />
+
+        <label htmlFor='password-login'>Password: </label>
+        <input required onChange={handleChange} type='password' name='password' id='password-login' />
+        <br />
+        {error ? <p>There was an error logging you in, please try again.</p> : null}
+
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }
