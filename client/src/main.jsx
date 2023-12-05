9377fbc27defbdcd9c4846be985710c50d827177
Vincent440 <vinceshury@gmail.com>
2023-12-05 16:25:11 -0500

    Added catch all error page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,13 @@ import App from './app.jsx'
 import Dashboard from './pages/dashboard'
 import Login from './pages/login.jsx'
 import SignUp from './pages/sign-up.jsx'
+import ErrorPage from './pages/error.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
